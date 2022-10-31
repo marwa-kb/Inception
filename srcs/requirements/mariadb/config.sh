d0aaacf397172888a2314829870a1abf78470e8a
MK <mkhabou@student.42.fr>
2022-10-31 20:07:05 +0100

    .. pid 1 ?

--- srcs/requirements/mariadb/config.sh
+++ srcs/requirements/mariadb/config.sh
@@ -1,4 +1,17 @@
-/etc/init.d/mariadb setup
-cd 'usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
-sleep 5
-mysql
+#!/bin/sh
+
+#/etc/init.d/mariadb setup
+#/usr/bin/mysql_install_db --user='mysql' --datadir='/var/lib/mysql'
+
+#/usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
+
+#/etc/init.d/mariadb start
+
+
+#/etc/init.d/mariadb setup
+#/etc/init.d/mariadb stop
+
+/usr/bin/mysql_install_db --user='mysql' --datadir='/var/lib/mysql'
+cd /usr ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
+
+
