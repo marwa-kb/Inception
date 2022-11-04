c2a25066ceb490bfdedbd75de52f1da3e75ba887
MK <mkhabou@student.42.fr>
2022-11-04 17:59:58 +0100

    better mariadb

--- srcs/requirements/mariadb/config.sh
+++ srcs/requirements/mariadb/config.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-#/etc/init.d/mariadb setup
-#/usr/bin/mysql_install_db --user='mysql' --datadir='/var/lib/mysql'
-
-#/usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
-
-#/etc/init.d/mariadb start
-
-
-#/etc/init.d/mariadb setup
-#/etc/init.d/mariadb stop
-
 /usr/bin/mysql_install_db --user='mysql' --datadir='/var/lib/mysql'
-cd /usr ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
-
-
+cd /usr ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
