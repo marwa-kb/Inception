c65c4c471cad4d041ccb4a0795d26c0014dd9b2e
Marwa Khabou <mkhabou@student.42.fr>
2022-11-12 17:45:20 +0100

    better i think

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#mysql -u root -p:password < /config.sql
 /usr/bin/mysql_install_db --user='mysql' --datadir='/var/lib/mysql'
 cd /usr ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
-mysql -u root -p:password < /config.sql
+mysql << EOF
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@localhost;
+FLUSH PRIVILEGES;
+EOF
