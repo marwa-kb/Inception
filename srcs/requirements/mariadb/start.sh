e1b0315fa9d7415be6aa469910018c97d7f79557
MK <mkhabou@student.42.fr>
2022-11-15 13:34:43 +0100

    essais + avancee wordpress

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-#mysql -u root -p:password < /config.sql
+#mysql -u root -ppassword < /config.sql
+
 /usr/bin/mysql_install_db --user='mysql' --datadir='/var/lib/mysql'
-cd /usr ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
-mysql << EOF
-CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@localhost;
-FLUSH PRIVILEGES;
-EOF
+
+cd /usr ;
+
+touch tmp.sql
+echo "CREATE DATABASE $MYSQL_DATABASE;" > tmp
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> tmp
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@%;" >> tmp
+echo "FLUSH PRIVILEGES;" >> tmp
+
+#some configuration to disable to connect without password
+
+< tmp /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+
+rm tmp
